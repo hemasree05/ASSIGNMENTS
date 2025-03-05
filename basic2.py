a50ba6e196fd26bd88c12ef69316aaec4be3c75e
hemasree05 <125129155+hemasree05@users.noreply.github.com>
2025-03-05 19:35:20 +0530

    Add files via upload

---
+++ basic2.py
@@ -0,0 +1,13 @@
+print("This is a single-line comment in Python.")
+
+"""
+Multi-line comment:
+You can use triple double-quotes to create multi-line comments 
+"""
+print("The above statments represent multi-line comments.")
+
+'''
+You can also use triple single-quotes
+for multi-line comments as well.
+'''
+print("The above shown statements represents another way to write multi-line comments.")
