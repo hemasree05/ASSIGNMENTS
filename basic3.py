64c2fd65ce5e70d7d7edca7197359eecca657727
hemasree05 <125129155+hemasree05@users.noreply.github.com>
2025-03-05 19:35:40 +0530

    Add files via upload

---
+++ basic3.py
@@ -0,0 +1,12 @@
+integer_variable = 10  
+boolean_variable = True
+character_variable = 'A'  
+float_variable = 3.14  
+double_variable = 1.23456789 
+
+#printing on the console in python
+print("Integer variable:", integer_variable)
+print("Boolean variable:", boolean_variable)
+print("Character variable:", character_variable)
+print("Float variable:", float_variable)
+print("Double variable:", double_variable)
